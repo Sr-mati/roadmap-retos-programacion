5e3bbc9fa7003762bc73b94c0bc4f339a5183a81
Sr-mati <matias77isaza@gmail.com>
2024-07-24 17:37:12 -0500

    Create Sr-mati.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Sr-mati.py
@@ -0,0 +1,34 @@
+# https://python.org
+
+# Comentario en una línea
+
+"""
+esto también es
+un comentario
+en varias líneas
+"""
+
+'''
+esto también es
+un comentario
+en varias líneas
+'''
+
+my_variable = "mi variable"
+my_variable = "nuevo valor de mi variable"
+
+MY_CONSTANT = "mi constante"  # por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "mi cadena de texto"
+my_other_string = 'mi otra cadena de texto'
+
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
